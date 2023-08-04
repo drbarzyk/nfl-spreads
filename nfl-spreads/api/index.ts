bc70d3f6d054870a3755d581fac9c32169272a37
Dan Barzyk <danbarzyk@mbp.local>
2023-08-04 17:00:41 -0400

    /api/ endpoint updated

--- nfl-spreads/api/index.ts
+++ nfl-spreads/api/index.ts
@@ -4,12 +4,7 @@ import Game from '../mongoose/schemas/Game';
 
 export default async (req: any, res: any) => {
     try {
-        // Fetch the data from the ESPN CDN
-        const response = await axios.get(process.env.ESPN_URI!);
-        const data = response.data; // Adjust this line based on the actual structure of the response
-
-        res.status(200).send(data.content.sbData)
-        // res.status(201).send('Games imported successfully');
+        res.status(200).send("NFL Spread Showdown API")
     } catch (err) {
         console.error(err);
         res.status(500).send('Error importing games');
